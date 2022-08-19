997d1d6a47a6e36f0545ade6dc370a00a36d336e
yugandharnagulla <yugandharnagulla@gmail.com>
2022-08-19 15:11:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import  datetime,random as rn
+import sys
+import webbrowser as wb
+print("today date is:",datetime.date.today())
+print("random no is",rn.randint(1,100))
+print(sys.path)
+wb.open("https://github.com/")
